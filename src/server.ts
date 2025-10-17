87acec9a745fe86395d18a7c6308e44860e9eb26
Adam Cameron <cameron.adam+github@gmail.com>
2025-10-17 09:40:16 +0100

    FL-39: improve separation of concerns in 'webserver' config

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,4 @@
 import express from 'express'
-export const app = express()
-
-app.get('/', function (req, res) {
-  return res.send("G'day World")
-})
-
-app.listen(3000, function () {
-  console.log('Listening on port 3000')
-})
+const app = express()
+app.get('/', (req, res) => res.send("G'day World"))
+export { app }
